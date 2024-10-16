e35a164bac07a1557d5073e5eb5035e2be8a7d6d
hliosone <sstelcher@protonmail.com>
2024-10-16 22:13:58 +0200

    JDBC Learning

---
+++ src/test/java/dev/hliosone/jdbclearning/HelloJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hliosone.jdbclearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
